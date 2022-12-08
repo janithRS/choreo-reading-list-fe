b82cb371ca67ca656abedbdbd66d12bddb657e7b
janithrs <janithravindu@gmail.com>
2022-12-08 15:12:00 +0530

    Show user name in index

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { Book } from "./api/books/types/book";
 import groupBy from "lodash/groupBy";
 import AddItem from "./components/modal/fragments/add-item";
 import { deleteBooks } from "./api/books/delete-books";
-import { useAuthContext } from "@asgardeo/auth-react";
+import { BasicUserInfo, useAuthContext } from "@asgardeo/auth-react";
 import { Dictionary } from "lodash";
 
 export function classNames(...classes: string[]) {
@@ -16,15 +16,10 @@ export default function App() {
   const [readList, setReadList] = useState<Dictionary<Book[]> | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const {
-    signIn,
-    signOut,
-    getAccessToken,
-    state,
-    isAuthenticated,
-    revokeAccessToken,
-  } = useAuthContext();
+  const { signIn, signOut, getAccessToken, isAuthenticated, getBasicUserInfo } =
+    useAuthContext();
   const [signedIn, setSignedIn] = useState(false);
+  const [user, setUser] = useState<BasicUserInfo | null>(null);
 
   useEffect(() => {
     async function signInCheck() {
@@ -35,11 +30,19 @@ export default function App() {
     signInCheck().then((res) => {
       if (res) {
         getReadingList();
+        getUser();
       } else {
         console.log("User has not signed in");
       }
     });
   }, []);
+
+  async function getUser() {
+    setIsLoading(true);
+    const userResponse = await getBasicUserInfo();
+    setUser(userResponse);
+    setIsLoading(false);
+  }
 
   async function getReadingList() {
     setIsLoading(true);
@@ -81,22 +84,12 @@ export default function App() {
   }
 
   return (
-    <div className="w-full max-w-lg px-2 py-16 sm:px-0 mb-20">
-      <div className="flex justify-between">
-        <p className="text-4xl text-white mb-3 font-bold">Reading List</p>
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      {user && (
         <>
-          <button
-            className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
-            onClick={() => setIsOpen(true)}
-          >
-            + Add New
-          </button>
-          <button
-            className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
-            onClick={() => getReadingList()}
-          >
-            Refresh List
-          </button>
+          <p className="text-2xl text-white mb-3 font-bold">
+            Hi {user.username}!
+          </p>
           <button
             className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
             onClick={() => signOut()}
@@ -104,69 +97,88 @@ export default function App() {
             Logout
           </button>
         </>
-      </div>
-      {readList && (
-        <Tab.Group>
-          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-            {Object.keys(readList).map((val) => (
-              <Tab
-                key={val}
-                className={({ selected }) =>
-                  classNames(
-                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                    selected
-                      ? "bg-white shadow"
-                      : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-                  )
-                }
-              >
-                {val}
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="mt-2">
-            {Object.values(readList).map((books: Book[], idx) => (
-              <Tab.Panel
-                key={idx}
-                className={
-                  isLoading
-                    ? classNames(
-                        "rounded-xl bg-white p-3 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2 animate-pulse"
-                      )
-                    : classNames(
-                        "rounded-xl bg-white p-3 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-                      )
-                }
-              >
-                <ul>
-                  {books.map((book) => (
-                    <div className="flex justify-between">
-                      <li key={book.id} className="relative rounded-md p-3">
-                        <h3 className="text-sm font-medium leading-5">
-                          {book.name}
-                        </h3>
-
-                        <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                          <li>{book.author}</li>
-                          <li>&middot;</li>
-                        </ul>
-                      </li>
-                      <button
-                        className="float-right bg-red-500 text-white rounded-md self-center text-xs p-2 mr-2"
-                        onClick={() => handleDelete(book.id!)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  ))}
-                </ul>
-              </Tab.Panel>
-            ))}
-          </Tab.Panels>
-        </Tab.Group>
       )}
-      <AddItem isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div className="w-full max-w-lg px-2 py-16 sm:px-0 mb-20">
+        <div className="flex justify-between">
+          <p className="text-4xl text-white mb-3 font-bold">Reading List</p>
+          <>
+            <button
+              className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
+              onClick={() => setIsOpen(true)}
+            >
+              + Add New
+            </button>
+            <button
+              className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
+              onClick={() => getReadingList()}
+            >
+              Refresh List
+            </button>
+          </>
+        </div>
+        {readList && (
+          <Tab.Group>
+            <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+              {Object.keys(readList).map((val) => (
+                <Tab
+                  key={val}
+                  className={({ selected }) =>
+                    classNames(
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                      selected
+                        ? "bg-white shadow"
+                        : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  {val}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className="mt-2">
+              {Object.values(readList).map((books: Book[], idx) => (
+                <Tab.Panel
+                  key={idx}
+                  className={
+                    isLoading
+                      ? classNames(
+                          "rounded-xl bg-white p-3 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2 animate-pulse"
+                        )
+                      : classNames(
+                          "rounded-xl bg-white p-3 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                        )
+                  }
+                >
+                  <ul>
+                    {books.map((book) => (
+                      <div className="flex justify-between">
+                        <li key={book.id} className="relative rounded-md p-3">
+                          <h3 className="text-sm font-medium leading-5">
+                            {book.name}
+                          </h3>
+
+                          <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                            <li>{book.author}</li>
+                            <li>&middot;</li>
+                          </ul>
+                        </li>
+                        <button
+                          className="float-right bg-red-500 text-white rounded-md self-center text-xs p-2 mr-2"
+                          onClick={() => handleDelete(book.id!)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    ))}
+                  </ul>
+                </Tab.Panel>
+              ))}
+            </Tab.Panels>
+          </Tab.Group>
+        )}
+        <AddItem isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
     </div>
   );
 }
